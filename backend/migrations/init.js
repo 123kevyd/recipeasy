1b9faf8eee18217c01cd6d8b35f0235e2c14044f
Sebastian <araneda3@myumanitoba.ca>
2022-02-11 14:10:40 -0600

    Documenting sequelize commands and creating db models

---
+++ backend/migrations/init.js
@@ -0,0 +1,47 @@
+'use strict';
+module.exports = {
+  async up(queryInterface, Sequelize) { // create a specific table (apply the migration)
+    await queryInterface.createTable('user', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      email: {
+        type: Sequelize.STRING
+      },
+      created_at: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updated_at: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+
+    await queryInterface.createTable('video', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      title: {
+        type: Sequelize.STRING
+      },
+      created_at: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updated_at: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) { // undo the current migration
+    await queryInterface.dropAllTables();
+  }
+};
