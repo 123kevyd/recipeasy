b580bc91392c182a8870c367459170c8302fc2ed
Vinh Tran <vinhtran@Vinhs-MacBook-Pro.local>
2022-02-23 15:09:20 -0600

    Modify table name to be singular

--- backend/migrations/20220217230113-create-recipe.js
+++ backend/migrations/20220217230113-create-recipe.js
@@ -1,7 +1,7 @@
 'use strict';
 module.exports = {
   async up(queryInterface, Sequelize) {
-    await queryInterface.createTable('recipes', {
+    await queryInterface.createTable('recipe', {
       id: {
         allowNull: false,
         autoIncrement: true,
--- backend/migrations/20220217230512-create-ingredient.js
+++ backend/migrations/20220217230512-create-ingredient.js
@@ -1,7 +1,7 @@
 'use strict';
 module.exports = {
   async up(queryInterface, Sequelize) {
-    await queryInterface.createTable('ingredients', {
+    await queryInterface.createTable('ingredient', {
       id: {
         allowNull: false,
         autoIncrement: true,
