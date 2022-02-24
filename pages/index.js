1ef12388b53ddb1aaeed8183e9065c76b47302e7
123kevyd <fpvfpvv3@gmail.com>
2022-02-24 16:21:13 -0600

    Revert "Initial testing"
    This reverts commit bc73b7dfae4c19e9799de4b285aaf7a3bf20274e.

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,6 @@ import { useState, useEffect } from 'react'
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
-import BrowseRecipes from './browse_recipes/[browse_recipes]'
 
 export default function Home() {
 	const [ingredients, setIngredients] = useState(null)
@@ -15,30 +14,18 @@ export default function Home() {
 			})
 	}, [])
 
-	// if (!ingredients) return <p>No profile data</p>
-	// const list = ingredients.map((ingredient) =>
-	// <li key={ingredient.id}>
-	// 	{ingredient.name}
-	// </li>
-	// );
-	let list
-	if (ingredients) {
-		list = ingredients.map((ingredient) =>
-			<li key={ingredient.id}>
-				{ingredient.name}
-			</li>
-		);
-	} else {
-		list = <p>No profile data</p>
-	}
-	
+	if (!ingredients) return <p>No profile data</p>
+
+	const list = ingredients.map((ingredient) =>
+		<li key={ingredient.id}>
+			{ingredient.name}
+		</li>
+	);
 
 	return (
 		<div>
-			<BrowseRecipes></BrowseRecipes>
 			<div>Hello world</div>
 			list
-
 		</div>
 	)
 }
