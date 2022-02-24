5a21ceeae4e33860ce8880507232b5a19f2b7814
wonge1 <edmundwong127@gmail.com>
2022-02-23 22:02:43 -0600

    -installing dependencies for testing backend

---
+++ backend/tests/setup_test.js
@@ -0,0 +1,14 @@
+const chai = require("chai");
+const sinon = require("sinon");
+const expect = chai.expect;
+const { rating } = require("../models/rating");
+const RatingController = require("../controllers/rating_controller");
+describe("Rating Controller", function() {
+  const stubValue = {
+  };
+  describe("create", function() {
+    it("should add a new user to the db", async function() {
+      expect(true).to.equal(true);
+    });
+  });
+});
