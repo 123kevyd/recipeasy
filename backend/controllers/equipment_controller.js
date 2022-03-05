c19d9c090f513f49fd4d9e58984bb9b7a6cc2232
wonge1 <edmundwong127@gmail.com>
2022-03-05 03:53:44 -0600

    Revert "Revert "Merge branch 'dev' of https://github.com/123kevyd/recipeasy into dev""
    This reverts commit 0a22ecc544a8aaba0e6afdf724ab534a5e03e79a.

--- backend/controllers/equipment_controller.js
+++ backend/controllers/equipment_controller.js
@@ -4,6 +4,14 @@ const equipment = db.equipment;
 exports.get = async(req,res) => {
     const result = await equipment.findAll();
     return result;
-    //console.log(result);
+}
+
+exports.post = async(req, res) => {
+    if(req.body.data.name) {
+        const result = await equipment.create({name: req.body.data.name});
+        return result;
+    }else {
+        //Missing the new equipment's name in body
+    }
 }
 
