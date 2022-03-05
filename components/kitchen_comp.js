b23c94dfcfd9728f5b7bf1b0cab20b5390deb719
RightMakesMight <xianmasur@gmail.com>
2022-03-04 19:20:19 -0600

    add kitchen component and stub subcomponents

--- components/kitchen_comp.js
+++ components/kitchen_comp.js
@@ -1,3 +1,32 @@
-export default function Kitchen() {
+import Ingredients from './ingredients_comp'
+import Restrictions from './restrictions_comp'
+import Equipment from './equipment_comp'
+import MyRecipes from './my_recipes_comp'
+import Box from '@mui/material/Box'
+export default function Kitchen(props) {
+	const kitchen = (
+		<Box sx={{ 
+			display: 'flex',
+			flexDirection: 'row',
+			flexWrap: 'wrap',
+			border: '1px solid grey',
+			}}>
+			<Ingredients
+				ingredients={props.ingredients}
+				myIngredients={props.myIngredients}
+			/>
+			<Equipment
+				equipment={props.equipment}
+				myEquipment={props.myEquipment}
+			/>
+			<Restrictions
+				restrictions={props.restrictions}
+				myRestrictions={props.myRestrictions}
+			/>
+			<MyRecipes
+				myRecipes={props.myRecipes}
+			/>
+		</Box>
+	)
 	return <div />
 }
