4c33ba3e275ca39b39343175d5ff993132bcc5b4
123kevyd <fpvfpvv3@gmail.com>
2022-03-05 03:07:11 -0600

    24: Column Sorting almost finished and other code improvements

--- components/cookbook.js
+++ components/cookbook.js
@@ -6,10 +6,10 @@ import Filter from './filter_comp'
 import RecipeTable from './recipe_table_comp';
 
 class Cookbook extends Component {
-	state = { recipeOpen: false }
 
 	tempRecipes = [{
-		title: "Easy Waffles",
+		id: 1,
+		title: "HARD Waffles",
 		description: "Consider this your new, go-to waffle recipe when you want to start your day off on a sweet note. No fussy steps or unexpected ingredients are required here, which means you can whip these up whenever your cravings hit.",
 		time: 30,
 		tags: ["Gluten Free", "Vegan", "Fish Free"],
@@ -30,10 +30,11 @@ class Cookbook extends Component {
 		equipment: ["Waffle Iron", "Whisk"],
 		reviews: [
 			{ rating: 5, difficulty: 3, description: "Loved this recipe, simple technique, on hand ingredients. My son is a very picky person, tastes all imperfections, and sensitive to over seasoning and sweetness. He loved this waffle and asked me to save the recipe, Which he has never done before" },
-			{ rating: 2, difficulty: 1, description: "I would not recommend 2 eggs! One egg is all you need, it took away from the fluffiness and was just too much w 2 I always use just 1, but decided to give 2 a try like the recipe called for, and my family definitely could tell the difference I won't do that again! Unless using 2 cups flour!" },
+			{ rating: 2, difficulty: 5, description: "I would not recommend 2 eggs! One egg is all you need, it took away from the fluffiness and was just too much w 2 I always use just 1, but decided to give 2 a try like the recipe called for, and my family definitely could tell the difference I won't do that again! Unless using 2 cups flour!" },
 			{ rating: 3, difficulty: 4, description: "Very easy recipe and it doesn’t make too many. I added approx. 1/4 more cup of flour and into the liquid ingredients I added 1 tsp vanilla. The recipe doesn't specify milk so I used a mix of 2% and whole." }
 		]
 	}, {
+		id: 2,
 		title: "Easy Waffles",
 		description: "Consider this your new, go-to waffle recipe when you want to start your day off on a sweet note. No fussy steps or unexpected ingredients are required here, which means you can whip these up whenever your cravings hit.",
 		time: 30,
@@ -62,13 +63,6 @@ class Cookbook extends Component {
 
 	currRecipe = this.tempRecipes[0]
 
-	handleToggleRecipe = () => {
-		let recipeOpen = this.state.recipeOpen
-		recipeOpen = !recipeOpen
-		this.setState({recipeOpen: recipeOpen})
-		console.log(this.state.recipeOpen)
-	}
-
 	render() { 
 		return (
 			<>
@@ -83,11 +77,6 @@ class Cookbook extends Component {
 				</Box>
 
 				<Button variant="contained" onClick={this.handleToggleRecipe}>Open View Recipe</Button>
-				<View_Recipe
-					onToggleRecipeView={this.handleToggleRecipe}
-					recipeOpen={this.state.recipeOpen}
-					recipe={this.currRecipe}
-				/>
 			</>
 		);
 	}
--- components/recipe_table_comp.js
+++ components/recipe_table_comp.js
@@ -1,7 +1,29 @@
-import { Chip, Table, TableCell, TableContainer, TableHead, TableRow } from '@mui/material';
+import { Chip, Stack, Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Typography, TableSortLabel, Button } from '@mui/material';
+import StarIcon from '@mui/icons-material/Star'
 import React, { Component } from 'react';
+import View_Recipe from "./view_recipe"
 
 class RecipeTable extends Component {
+	state = { 
+        recipeOpen: false,
+        currRecipe: this.props.recipes[0],
+        shownRecipes: this.formatRecipes(this.props.recipes),
+        sortInfo: [
+            { colName: "Title", dir: 'asc', active: false },
+            { colName: "Time", dir: 'asc', active: false },
+            { colName: "Difficulty", dir: 'asc', active: false },
+            { colName: "Rating", dir: 'asc', active: false }
+        ]
+    }
+
+    formatRecipes(recipes) {
+        recipes.forEach((recipe) => {
+            recipe.rating = this.getAverageRating(recipe.reviews)
+            recipe.difficulty = this.getAverageDifficulty(recipe.reviews)
+        })
+        return recipes
+    }
+
     getAverageDifficulty(reviews) {
         let sum = 0
         Object.values(reviews).forEach( review => {
@@ -21,28 +43,107 @@ class RecipeTable extends Component {
         return tags.map( tag => <Chip key={tag} label={tag} />)
     }
 
+	handleToggleRecipe = (recipe) => {
+		let recipeOpen = this.state.recipeOpen
+		recipeOpen = !recipeOpen
+		this.setState({recipeOpen: recipeOpen})
+	}
+
+    compareRecipes(a, b, orderBy) {
+        if (b[orderBy] < a[orderBy])
+            return -1
+        if (b[orderBy] > a[orderBy])
+            return 1
+        return 0
+    }
+
+    getCompare (order, orderBy) {
+        return order === 'desc' ? (a, b) => this.compareRecipes(a, b, orderBy) : (a, b) => -(this.compareRecipes(a, b, orderBy));
+    }
+
+    handleSortChange = (colName) => {
+        let sortInfo = JSON.parse(JSON.stringify(this.state.sortInfo))
+        let index = -1
+        let allRecipes = JSON.parse(JSON.stringify(this.props.recipes))
+
+        sortInfo.forEach((elem, i) => {if (elem.colName === colName) index = i})
+        if (!sortInfo[index].active) {
+            sortInfo.forEach((elem) => {elem.active = false})
+            sortInfo[index].active = true
+            sortInfo[index].dir = 'asc'
+        } else if (sortInfo[index].dir === "asc") {
+            sortInfo.forEach((elem) => {elem.active = false})
+            sortInfo[index].active = true
+            sortInfo[index].dir = 'desc'
+        } else {
+            sortInfo.forEach((elem) => {elem.active = false})
+        }
+        
+        allRecipes.sort(this.getCompare(sortInfo[index].dir, sortInfo[index].colName))
+
+        this.setState({sortInfo: sortInfo})
+        this.setState({shownRecipes: this.formatRecipes(allRecipes)})
+    }
+
     render() { 
         return (
-            <TableContainer>
-                <Table>
-                    <TableHead>
-                        <TableCell>Name</TableCell>
-                        <TableCell>Time</TableCell>
-                        <TableCell>Difficulty</TableCell>
-                        <TableCell>Rating</TableCell>
-                        <TableCell>Tags</TableCell>
-                    </TableHead>
-                    { this.props.recipes.map( recipe =>
-                        <TableRow key={recipe}>
-                            <TableCell>{recipe.title}</TableCell>   
-                            <TableCell>{recipe.time}</TableCell>
-                            <TableCell>{this.getAverageDifficulty(recipe.reviews)}</TableCell>
-                            <TableCell>{this.getAverageRating(recipe.reviews)}</TableCell>
-                            <TableCell>{this.getTags(recipe.tags)}</TableCell>
-                        </TableRow>
-                    )}
-                </Table>
-            </TableContainer>
+            <>
+                <TableContainer>
+                    <Table>
+                        <TableHead>
+                            <TableRow>
+                                {this.state.sortInfo.map(column =>
+                                    <TableCell key={column.colName}>
+                                        <TableSortLabel
+                                            active={column.active}
+                                            direction={column.dir}
+                                            onClick={() => this.handleSortChange(column.colName)}
+                                        >
+                                            {column.colName}
+                                        </TableSortLabel>
+                                    </TableCell>
+                                )}
+                                <TableCell>
+                                    Tags
+                                </TableCell>
+                            </TableRow>
+                        </TableHead>
+                        <TableBody>
+                            { this.state.shownRecipes.map( recipe =>
+                                <TableRow key={recipe.id} onClick={(recipe) => this.handleToggleRecipe(recipe)}>
+                                    <TableCell>{recipe.title}</TableCell>   
+                                    <TableCell>{recipe.time} mins</TableCell>
+                                    <TableCell>
+                                        { <Stack direction='row'>
+                                            <Typography variant='body1'>
+                                            {recipe.difficulty}&nbsp;
+                                            </Typography>
+                                            <StarIcon />
+                                        </Stack> }
+                                    </TableCell>
+                                    <TableCell>
+                                        <Stack direction='row'>
+                                            <Typography variant='body1'>
+                                                {recipe.rating}&nbsp;
+                                            </Typography>
+                                            <StarIcon />
+                                        </Stack>
+                                        
+                                    </TableCell>
+                                    <TableCell>
+                                        {this.getTags(recipe.tags)}
+                                    </TableCell>
+                                </TableRow>
+                            )}
+                        </TableBody>
+                    </Table>
+                </TableContainer>
+                <View_Recipe
+                    onToggleRecipeView={this.handleToggleRecipe}
+                    recipeOpen={this.state.recipeOpen}
+                    recipe={this.state.currRecipe}
+                />
+            </>
         );
     }
 }
