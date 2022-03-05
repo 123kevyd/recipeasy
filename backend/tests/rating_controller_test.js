8995f7df31a59ad3970285c8e2977b6e8921b85d
wonge1 <edmundwong127@gmail.com>
2022-03-05 02:37:35 -0600

    -finsihing equipment tests  -deleting pract test

--- backend/tests/rating_controller_test.js
+++ backend/tests/rating_controller_test.js
@@ -6,39 +6,38 @@ const rating = db.rating;
 const RatingController = require("../controllers/rating_controller");
 
 describe("Rating Controller", function() {
+  
+    const fiveStarReview = {
+        stars: 5,
+        review: "Best Movie"
+    };
 
-  const fiveStarReview = {
-    stars: 5,
-    review: "Best Movie"
-  };
-
-  const oneStarReview = {
+    const oneStarReview = {
       stars: 1,
       review: "Worst Movie"
-  }
-  
-  const request = {
-    data: {
-        primaryKey: 5
     }
-  };
+  
+    const request = {
+        data: {
+            primaryKey: 5
+        }
+    };
 
 
-  describe("Testing Get Function", async function() {
-    it("Should get different objects based on request input", async function() {
-        const mockMethod = sinon.stub(rating, "findByPk");
-        mockMethod.withArgs(5).returns(fiveStarReview);
-        mockMethod.withArgs(1).returns(oneStarReview);
+    describe("Testing Get Function", async function() {
+        it("Should get different objects based on request input", async function() {
+            const mockMethod = sinon.stub(rating, "findByPk");
+            mockMethod.withArgs(5).returns(fiveStarReview);
+            mockMethod.withArgs(1).returns(oneStarReview);
 
-        toCheck = await RatingController.get(request);
-        expect(toCheck.stars).to.be.equal(5);
-        expect(toCheck.review).to.be.equal("Best Movie");
+            toCheck = await RatingController.get(request);
+            expect(toCheck.stars).to.be.equal(5);
+            expect(toCheck.review).to.be.equal("Best Movie");
 
-        request.data.primaryKey = 1;
-        toCheck = await RatingController.get(request);
-        expect(toCheck.stars).to.be.equal(1);
-        expect(toCheck.review).to.be.equal("Worst Movie");
+            request.data.primaryKey = 1;
+            toCheck = await RatingController.get(request);
+            expect(toCheck.stars).to.be.equal(1);
+            expect(toCheck.review).to.be.equal("Worst Movie");
+        });
     });
-  });
 });
-//test a connection db
--- backend/tests/user_controller_test.js
+++ backend/tests/user_controller_test.js
@@ -6,83 +6,81 @@ const user = db.user;
 const UserController = require("../controllers/user_controller");
 
 describe("User Controller", function() {
-  const firstUser = {
-    username: "First Guy",
-    ingredients: "one two three",
-    equipment: "bowl",
-    restrictions: "none"
-  };
-  const secondUser = {
-    username: "Second Girl",
-    ingredients: "three two one",
-    equipment: "none",
-    restrictions: "lactose"
-  };
-
-  describe("Testing Get Function", async function() {
-    var request = {
-      query: {
-          uid: 5
-      }
+    const firstUser = {
+        username: "First Guy",
+        ingredients: "one two three",
+        equipment: "bowl",
+        restrictions: "none"
     };
-    it("Should get different objects based on request input", async function() {
-        const mockMethod = sinon.stub(user, "findByPk");
-        mockMethod.withArgs(5).returns(firstUser);
-        mockMethod.withArgs(1).returns(secondUser);
-
-        toCheck = await UserController.get(request);
-        expect(toCheck.username).to.be.equal("First Guy");
-        expect(toCheck.restrictions).to.be.equal("none");
-
-        request.query.uid = 1;
-        toCheck = await UserController.get(request);
-        expect(toCheck.username).to.be.equal("Second Girl");
-        expect(toCheck.restrictions).to.be.equal("lactose");
-    });
-  });
-
-  describe("Testing Post Function", async function() {
-    var request = {
-      query: {
-        uid: 1
-      },
-      body: {
-        data: {
-            username: null,
-            ingredients: null,
-            equipment: null,
-            restrictions: null
-        }
-      }
+    const secondUser = {
+        username: "Second Girl",
+        ingredients: "three two one",
+        equipment: "none",
+        restrictions: "lactose"
     };
 
-    it("Should Call Nothing Due to Bad Request", async function() {
-      toCheck = await UserController.post(request);
-      expect(toCheck).to.be.equal(undefined);
+    describe("Testing Get Function", async function() {
+        var request = {
+          query: {
+              uid: 5
+          }
+        };
+        it("Should get different objects based on request input", async function() {
+            const mockMethod = sinon.stub(user, "findByPk");
+            mockMethod.withArgs(5).returns(firstUser);
+            mockMethod.withArgs(1).returns(secondUser);
+
+            toCheck = await UserController.get(request);
+            expect(toCheck.username).to.be.equal("First Guy");
+            expect(toCheck.restrictions).to.be.equal("none");
+
+            request.query.uid = 1;
+            toCheck = await UserController.get(request);
+            expect(toCheck.username).to.be.equal("Second Girl");
+            expect(toCheck.restrictions).to.be.equal("lactose");
+        });
     });
-    
-    it("Should Call Create Method", async function() {
-        request.body.data.username = "username";
 
-        const mockMethod = sinon.stub(user, "create");
-        mockMethod.returns("Success");
+    describe("Testing Post Function", async function() {
+        var request = {
+            query: {
+                uid: 1
+            },
+            body: {
+              data: {
+                  username: null,
+                  ingredients: null,
+                  equipment: null,
+                  restrictions: null
+              }
+            }
+        };
 
-        toCheck = await UserController.post(request);
-        expect(toCheck).to.be.equal("Success");
+        it("Should Call Nothing Due to Bad Request", async function() {
+            toCheck = await UserController.post(request);
+            expect(toCheck).to.be.equal(undefined);
+        });
+      
+        it("Should Call Create Method", async function() {
+            request.body.data.username = "username";
 
+            const mockMethod = sinon.stub(user, "create");
+            mockMethod.returns("Create Method was Successfully Called");
+
+            toCheck = await UserController.post(request);
+            expect(toCheck).to.be.equal("Create Method was Successfully Called");
+        });
+
+        it("Should Call Update Method", async function() {
+            request.body.data.ingredients = "what";
+            request.body.data.restrictions = "when";
+            request.body.data.equipment = "who";
+
+            const mockMethod = sinon.stub(user, "update");
+            mockMethod.returns("Update Method was Successfully Called");
+
+            toCheck = await UserController.post(request);
+            expect(toCheck).to.be.equal("Update Method was Successfully Called");
+        });
     });
-
-    it("Should Call Update Method", async function() {
-        request.body.data.ingredients = "what";
-        request.body.data.restrictions = "when";
-        request.body.data.equipment = "who";
-
-        const mockMethod = sinon.stub(user, "update");
-        mockMethod.returns("Success");
-
-        toCheck = await UserController.post(request);
-        expect(toCheck).to.be.equal("Success");
-    });
-  });
-  
 });
