a753beb603e6770b2fd4ae96194d064bc555ef7e
RightMakesMight <xianmasur@gmail.com>
2022-03-06 14:16:33 -0600

    created restrictions comp by copying ingredients, should combine

--- components/restrictions_comp.js
+++ components/restrictions_comp.js
@@ -1,3 +1,50 @@
-export default function Restrictions() {
-	return <div />
+import { useState, useEffect } from "react"
+import EntryDropdown from "./entry_dropdown_comp"
+import KitchenList from "./kitchen_list_comp"
+import Stack from "@mui/material/Stack"
+
+export default function Restrictions(props) {
+	//const restrictions = props.restrictions
+
+	const [ myRestrictions, setMyRestrictions ] = useState(props.myRestrictions)
+
+	const getDropdownList = () => {
+		return props.restrictions.filter( restriction1 => {
+			const found = myRestrictions.some( restriction2 =>
+				restriction1.title === restriction2.title
+			)
+			//if(found){
+				//console.log(`restriction ${restriction1.title} matched`)
+			//}else{
+				//console.log(`restriction ${restriction1.title} didn't match`)
+			//}
+			return ! found
+		})
+	}
+
+	const [ dropdownList, setDropdownList ] = useState(getDropdownList())
+
+	const restrictionSelected = (event, value) => {
+		setMyRestrictions(myRestrictions.concat([value]))
+		setDropdownList(getDropdownList())
+	}
+
+	const deleteIngredient = (restriction1) => {
+		setMyRestrictions(myRestrictions.filter(restriction2 => {
+			return restriction1.title !== restriction2.title
+		}))
+		setDropdownList(getDropdownList())
+	}
+
+	return (
+		<Stack spacing={2} sx={{ width: 300, padding: 3, border: '1px blue solid', margin: 3 }}>
+			<EntryDropdown
+				items={getDropdownList()}
+				handler={restrictionSelected}
+			/>
+			<KitchenList items={myRestrictions} delHandler={deleteIngredient}/>
+		</Stack>
+	)
+			
 }
+
