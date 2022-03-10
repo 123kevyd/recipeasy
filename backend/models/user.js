29c2d68aa0cc7ff1f0dc1a8f6a732053a3af9ca0
RightMakesMight <xianmasur@gmail.com>
2022-03-10 17:27:14 -0600

    fixing spacing

--- backend/models/user.js
+++ backend/models/user.js
@@ -20,7 +20,7 @@ module.exports = (sequelize, DataTypes) => {
     restrictions: DataTypes.STRING
   }, {
     sequelize,
-		timestamps: false,
+	timestamps: false,
     modelName: 'user',
     freezeTableName:  true,
   });
