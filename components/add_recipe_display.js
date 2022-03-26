11fb79dd9d0f0c4244fbb9f8b7ad169338cdb839
wonge1 <edmundwong127@gmail.com>
2022-03-26 17:04:37 -0500

    -setting up add recipes to take in desc and tag

--- components/add_recipe_display.js
+++ components/add_recipe_display.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
-import { Box, Button, ListItemText, TextField, Chip, ListItemIcon, createTheme } from '@mui/material'
+import { Box, Button, ListItemText, TextField, Chip, Card, CardHeader, CardContent, createTheme, Select, OutlinedInput, MenuItem, InputLabel, FormControl } from '@mui/material'
 import AddRecipeIngredients from './add_recipe_ingredients'
+import { maxHeight } from '@mui/system';
 
 class AddRecipeDisplay extends Component {
 	theme = createTheme({
@@ -13,22 +14,48 @@ class AddRecipeDisplay extends Component {
 		}
 	})
 
-    ingredients = [
-		{ name: "all-purpose flour", quantity: 1, unit: "cup"},
-		{ name: "sugar", quantity: 2 , unit: "tablespoon"},
-		{ name: "baking powder", quantity: 1, unit: "teaspoon"},
-		{ name: "salt", quantity: 0.25, unit: "teaspoon"},
-		{ name: "milk", quantity: 1, unit: "cup"},
-		{ name: "eggs", quantity: 2, unit: "large"},
-		{ name: "unslated butter (melted)", quantity: 4, unit: "tablespoon"}
-	];
+    state = {
+        newRecipe: {
+            "title": "",
+            "directions": [],
+            "ingredients": [{
+                unit: "",
+                quantitiy: 0,
+                name: ""
+            }],
+            "equipment": [],
+            "description": "",
+            "tags": [],
+            "time": 0
+        }
+    }
 
-    ingUnit = [
-        "cup",
-        "tablespoon",
-        "teaspoon"
-    ]
+    handleUpdateDescription(description) {
+        let updatedRecipe = JSON.parse(JSON.stringify(this.state.newRecipe));
+        updatedRecipe.description = description;
+        this.setState({newRecipe: updatedRecipe});
+    }
 
+    handleUpdateRestrictions(tags) {
+        let updatedTags = JSON.parse(JSON.stringify(this.state.newRecipe));
+        updatedTags.tags = typeof tags === 'string' ? tags.split(',') : tags;
+        this.setState({newRecipe: updatedTags});
+        
+    }
+    
+    restrictions = [
+        'Oliver Hansen',
+        'Van Henry',
+        'April Tucker',
+        'Ralph Hubbard',
+        'Omar Alexander',
+        'Carlos Abbott',
+        'Miriam Wagner',
+        'Bradley Wilkerson',
+        'Virginia Andrews',
+        'Kelly Snyder',
+    ];
+    /*
     formatAddRequest() {
         return {
             "data": {
@@ -40,10 +67,9 @@ class AddRecipeDisplay extends Component {
                 "details": null,
                 "author": 1
             }
-            
         }
     }
-
+    */
     async addRecipe(request) {
         const response = await fetch(`http://localhost:3000/api/recipes`, {
                 method: 'POST',
@@ -53,40 +79,98 @@ class AddRecipeDisplay extends Component {
                 },
                 body: JSON.stringify(request)
             });
-        
         console.log(response);
     }
 
     render() { 
         return (
             <Box
-            component="form"
-            sx={{
-                width: 1000,
-                margin: 2,
-                //'& .MuiTextField-root': { m: 1, width: '25ch' },
-                maxWidth: '100%'
-            }}
-            noValidate
-            autoComplete="off"
+            classes="wide-text"
             >
-                <div>
-                    <TextField
-                        required
-                        id="outlined-required"
-                        label="Recipe Name"
-                        fullWidth
-                    />
-                    <TextField
-                        width = "25"
-                        label="Total Servings" 
-                        id="outlined-number"
-                        margin = "normal"
-                        type="number"
-                    />
-                    <AddRecipeIngredients></AddRecipeIngredients>
-                </div>
-                <Button variant='contained' onClick={() => this.addRecipe(this.formatAddRequest())}>Submit</Button>
+                <TextField
+                    required
+                    label="Recipe Name"
+                    fullWidth
+                />
+                <Box sx={{display:'grid', gridTemplateColumns: 'repeat(2, 1fr)'}}>
+                    <Box>
+                        <Card>
+                            <CardHeader title="Description"/>
+                            <CardContent>
+                                <TextField
+                                    required
+                                    label="Description"
+                                    fullWidth
+                                    multiline
+                                    rows={5}
+                                    onChange={(event) => this.handleUpdateDescription(event.target.value)}
+                                />
+                            </CardContent>
+                        </Card>
+                        <Card>
+                            <CardHeader title="Restrictions"/>
+                            <CardContent>   
+                                <FormControl fullWidth>
+                                    <InputLabel id="restriction-label">Tags</InputLabel>
+                                    <Select
+                                        labelId='restriction-label'
+                                        MenuProps = {{
+                                            PaperProps: {
+                                            style: {
+                                                maxHeight: 180,
+                                                width: 250
+                                            }
+                                            }
+                                        }}
+                                        fullWidth
+                                        multiple
+                                        onChange={(event) => this.handleUpdateRestrictions(event.target.value)}
+                                        value={this.state.newRecipe.tags}
+                                        input={<OutlinedInput label="Chip" />}
+                                        renderValue={(selected) => (
+                                            <Box sx={{ display: 'flex', flexWrap: 'wrap', gap: 0.5 }}>
+                                            {selected.map((value) => (
+                                                <Chip key={value} label={value} />
+                                            ))}
+                                            </Box>
+                                        )}>
+                                        {this.restrictions.map((restriction) => (
+                                            <MenuItem
+                                            key={restriction}
+                                            value={restriction}
+                                            >
+                                                {restriction}
+                                            </MenuItem>
+                                        ))}        
+                                    </Select>
+                                </FormControl>
+                            </CardContent>
+                        </Card>
+                    </Box>
+                    <Box>
+                        <Card>
+                            <CardHeader title="Restrictions"/>
+                            <CardContent>
+                                {this.state.ingredients.map((ingredient)=>{
+                                    return (
+                                        <>
+                                        <Select>
+                                        </Select>
+                                        
+                                        
+                                        </>
+                                    );
+                                })}
+                            </CardContent>
+                        </Card>
+                    </Box>
+                </Box>
+                <TextField
+                    label="Total Servings" 
+                    type="number"
+                />
+                <AddRecipeIngredients></AddRecipeIngredients>
+                <Button variant='contained'>Submit</Button>
             </Box>
         );
     }
