1db09d932175250bcc471a7fad19a9f2aea36341
wonge1 <edmundwong127@gmail.com>
2022-03-27 03:56:46 -0500

    -add recipe module layout finished

--- components/add_recipe_display.js
+++ components/add_recipe_display.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Box, Button, ListItemText, TextField, Chip, Card, CardHeader, CardContent, createTheme, Select, OutlinedInput, MenuItem, InputLabel, FormControl } from '@mui/material'
+import { Box, Button, ListItemText, TextField, Chip, Card, CardHeader, CardContent, createTheme, Select, OutlinedInput, MenuItem, InputLabel, FormControl, Stack } from '@mui/material'
 import AddRecipeIngredients from './add_recipe_ingredients'
 import { maxHeight } from '@mui/system';
 
@@ -30,6 +30,42 @@ class AddRecipeDisplay extends Component {
         }
     }
 
+    handleAddIngredient() {
+        let addIngredient = JSON.parse(JSON.stringify(this.state.newRecipe));
+        addIngredient.ingredients.push({
+            unit: "",
+            quantitiy: 0,
+            name: ""
+        });
+        this.setState({newRecipe: addIngredient});
+    }
+
+    handleDeleteIngredient(index) {
+        let delIngredient = JSON.parse(JSON.stringify(this.state.newRecipe));
+        if (index > -1) {
+            delIngredient.ingredients.splice(index, 1); // 2nd parameter means remove one item only
+        }
+        this.setState({newRecipe: delIngredient});
+    }
+
+    handleUpdateIngredientName(ingredient, index) {
+        let updatedIngredient = JSON.parse(JSON.stringify(this.state.newRecipe));
+        updatedIngredient.ingredients[index].name = ingredient;
+        this.setState({newRecipe: updatedIngredient});
+    }
+
+    handleUpdateIngredientUnit(unit, index) {
+        let updatedIngredient = JSON.parse(JSON.stringify(this.state.newRecipe));
+        updatedIngredient.ingredients[index].unit = unit;
+        this.setState({newRecipe: updatedIngredient});
+    }
+
+    handleUpdateIngredientQuantity(quantitiy, index) {
+        let updatedIngredient = JSON.parse(JSON.stringify(this.state.newRecipe));
+        updatedIngredient.ingredients[index].quantity = quantitiy;
+        this.setState({newRecipe: updatedIngredient});
+    }
+
     handleUpdateDescription(description) {
         let updatedRecipe = JSON.parse(JSON.stringify(this.state.newRecipe));
         updatedRecipe.description = description;
@@ -55,21 +91,18 @@ class AddRecipeDisplay extends Component {
         'Virginia Andrews',
         'Kelly Snyder',
     ];
-    /*
-    formatAddRequest() {
-        return {
-            "data": {
-                "name": "testRecipe",
-                "instructions": "Whisk the thing",
-                "ingredients": ["egg"],
-                "equipment": ["whisk"],
-                "servings": 3,
-                "details": null,
-                "author": 1
-            }
-        }
-    }
-    */
+
+    ingredients = [
+        'egg',
+        'milk'
+    ];
+
+    unit = [
+        'teaspoon',
+        'tablespoon',
+        'cup'
+    ];
+
     async addRecipe(request) {
         const response = await fetch(`http://localhost:3000/api/recipes`, {
                 method: 'POST',
@@ -87,11 +120,15 @@ class AddRecipeDisplay extends Component {
             <Box
             classes="wide-text"
             >
-                <TextField
-                    required
-                    label="Recipe Name"
-                    fullWidth
-                />
+                <Card>
+                    <CardHeader title="Recipe Name"/>
+                    <CardContent>
+                        <TextField
+                            label="Title" 
+                            fullWidth
+                        />
+                    </CardContent>
+                </Card>
                 <Box sx={{display:'grid', gridTemplateColumns: 'repeat(2, 1fr)'}}>
                     <Box>
                         <Card>
@@ -149,15 +186,61 @@ class AddRecipeDisplay extends Component {
                     </Box>
                     <Box>
                         <Card>
-                            <CardHeader title="Restrictions"/>
+                            <CardHeader title="Time"/>
                             <CardContent>
-                                {this.state.ingredients.map((ingredient)=>{
+                                <TextField
+                                    label="Total Minutes" 
+                                    type="number"
+                                />
+                            </CardContent>
+                        </Card>
+                        <Card>
+                            <CardHeader title="Ingredients"/>
+                            <CardContent>
+                                {this.state.newRecipe.ingredients.map((ingredient, index)=>{
                                     return (
                                         <>
-                                        <Select>
-                                        </Select>
-                                        
-                                        
+                                            <Stack direction="row" spacing={3}>
+                                                <Select
+                                                    label="Ingredient"
+                                                    fullWidth
+                                                    onChange={(event) => this.handleUpdateIngredientName(event.target.value,index)}
+                                                    value={ingredient.name}
+                                                    >
+                                                    {this.ingredients.map((ingredient) => (
+                                                        <MenuItem
+                                                        key={ingredient}
+                                                        value={ingredient}
+                                                        >
+                                                            {ingredient}
+                                                        </MenuItem>
+                                                    ))}         
+                                                </Select>
+                                                <Select
+                                                    label="Unit"
+                                                    fullWidth
+                                                    onChange={(event) => this.handleUpdateIngredientUnit(event.target.value, index)}
+                                                    value={ingredient.unit}
+                                                    >
+                                                    {this.unit.map((unit) => (
+                                                        <MenuItem
+                                                        key={unit}
+                                                        value={unit}
+                                                        >
+                                                            {unit}
+                                                        </MenuItem>
+                                                    ))}         
+                                                </Select>
+                                                <TextField
+                                                    label="Qnty"
+                                                    type="number"
+                                                    onChange={(event) => this.handleUpdateIngredientQuantity(event.target.value, index)}
+                                                />
+                                                <Button variant='contained' onClick={() => this.handleDeleteIngredient(index)}>Delete</Button>
+                                            </Stack>
+                                            {this.state.newRecipe.ingredients.length - 1 === index && (
+                                                <Button variant='contained' onClick={() => this.handleAddIngredient()}>Add Ingredient</Button>
+                                            )}
                                         </>
                                     );
                                 })}
@@ -165,11 +248,6 @@ class AddRecipeDisplay extends Component {
                         </Card>
                     </Box>
                 </Box>
-                <TextField
-                    label="Total Servings" 
-                    type="number"
-                />
-                <AddRecipeIngredients></AddRecipeIngredients>
                 <Button variant='contained'>Submit</Button>
             </Box>
         );
