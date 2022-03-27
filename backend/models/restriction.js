64afb6aba2d3f9bee9ed082c30f82f1e1225fc76
Vinh Tran <vinhtran.k@gmail.com>
2022-03-27 13:35:38 -0500

    add capitals for consistency

--- backend/models/restriction.js
+++ backend/models/restriction.js
@@ -3,7 +3,7 @@ const {
   Model
 } = require('sequelize');
 module.exports = (sequelize, DataTypes) => {
-  class restriction extends Model {
+  class Restriction extends Model {
     /**
      * Helper method for defining associations.
      * This method is not a part of Sequelize lifecycle.
@@ -14,12 +14,12 @@ module.exports = (sequelize, DataTypes) => {
       this.belongsToMany(models.User, { through: 'User_Restriction' })
     }
   }
-  restriction.init({
+  Restriction.init({
     name: DataTypes.STRING
   }, {
     sequelize,
     modelName: 'restriction',
     freezeTableName: true
   });
-  return restriction;
+  return Restriction;
 };
