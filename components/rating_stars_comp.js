073f172b2dd673e33fc46f067a8a8c5d4f039970
123kevyd <fpvfpvv3@gmail.com>
2022-03-27 16:16:56 -0500

    Finished testing RatingStars

--- components/rating_stars_comp.js
+++ components/rating_stars_comp.js
@@ -1,13 +1,32 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';  
 import StarIcon from '@mui/icons-material/Star'
-import StarBorderIcon from '@mui/icons-material/StarBorder'
+import StarBorderIcon from '@mui/icons-material/StarBorder' 
+
+/**
+ * Used for ratings. Shows 5 stars with the specified number of stars filled
+ * @prop {number} stars the number of filled stars to be shown
+ */
 
 class RatingStars extends Component {
+    constructor(props) {
+        super(props);
+        
+        if (!this.props || !this.props.stars && this.props.stars !== 0) {
+            throw new Error("required stars prop not being passed\n");
+        } else if (typeof this.props.stars !== 'number' ) {
+            throw new Error("Stars must be a number\n");
+        } else if (this.props.stars > 5 || this.props.stars < 0) {
+            throw new Error("Stars must be between 0 and 5 inclusive")
+        }
+    }
+
     range = [0, 1, 2, 3, 4]
+
     render() { 
         return (
             <>
-                { this.range.map( num => {
+                {this.range.map( num => {
                     return this.props.stars > num ? <StarIcon key={num} /> : <StarBorderIcon key={num} />
                 })}
             </>
--- pages/index.js
+++ pages/index.js
@@ -4,6 +4,7 @@ import FormControl from '@material-ui/core/FormControl'
 import InputLabel from '@material-ui/core/InputLabel'
 import Input from '@material-ui/core/Input'
 import Button from '@material-ui/core/Button'
+import RatingStars from '../components/rating_stars_comp'
 //import Head from 'next/head'
 //import Image from 'next/image'
 //import styles from '../styles/Home.module.css'
@@ -31,6 +32,7 @@ export default function Login(props) {
 
 	return (
 		<div className="Login">
+			<RatingStars stars={0} />
 			<form className="loginForm">
 				<InputLabel>
 					User Name
