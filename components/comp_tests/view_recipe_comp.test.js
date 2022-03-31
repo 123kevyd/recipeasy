09f4798bd910fcba3fdf69377d6325cca5a2ccd1
123kevyd <fpvfpvv3@gmail.com>
2022-03-31 02:40:35 -0500

    Removed old. broken import

--- components/comp_tests/view_recipe_comp.test.js
+++ components/comp_tests/view_recipe_comp.test.js
@@ -1,5 +1,4 @@
 import { fireEvent, render, screen } from '@testing-library/react'
-import userEvent from '@testing-library/user-event'
 import React from 'react';
 import '@testing-library/jest-dom/extend-expect';
 import ViewRecipe from '../view_recipe';
