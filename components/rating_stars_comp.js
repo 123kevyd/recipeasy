cfe566c2d68927adf6ef6f88f5d6c3925265b65e
123kevyd <fpvfpvv3@gmail.com>
2022-03-31 03:06:31 -0500

    updated ci console.log debug

--- components/rating_stars_comp.js
+++ components/rating_stars_comp.js
@@ -13,7 +13,8 @@ class RatingStars extends Component {
         super(props);
         
         if (!this.props || (!this.props.stars && this.props.stars !== 0)) {
-            throw new Error("required stars prop not being passed\n" + this.props + "\n");
+            throw new Error("required stars prop not being passed\n" + this.props + "\n" + this.props.stars + "\n");
+            
         } else if (typeof this.props.stars !== 'number' ) {
             throw new Error(`Stars must be a number - Is: ${this.props.stars} (${typeof this.props.stars})\n`);
         } else if (this.props.stars > 5 || this.props.stars < 0) {
