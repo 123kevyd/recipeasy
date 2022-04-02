0ed217015d5c80ed15b3082422db6f8a2a079db4
wonge1 <edmundwong127@gmail.com>
2022-04-02 13:37:47 -0500

    -isolating backend tests for fix (will revert)

--- components/comp_tests/recipe_rating_display_comp.test.js
+++ components/comp_tests/recipe_rating_display_comp.test.js
@@ -1,3 +1,4 @@
+/*
 import { render, screen } from '@testing-library/react'
 import React from 'react';
 import RecipeRatingDisplay from '../recipe_rating_display'
@@ -8,3 +9,4 @@ test("test one", () => {
     const element = screen.getByText('Reviews:')
     expect(element).toBeInTheDocument()
 })
+*/
