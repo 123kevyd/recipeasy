1ab7995f8dfd503c7b20449d51ab80d9e8caf860
RightMakesMight <xianmasur@gmail.com>
2022-04-09 15:21:00 -0500

    fix item not added to user data

--- store/user_store.js
+++ store/user_store.js
@@ -58,9 +58,12 @@ export const userStore = create((set, get) => ({
 				toggleLoading(newItem, set, get)
 				return null
 			}
-			data = await res.json()[0]
+			data = (await res.json())[0]
+			console.log('posted data')
+			console.log(data)
 			toggleLoading(newItem, set, get)
 		}
+		console.log("putting to user")
 		toggleLoading(data, set, get)
 		var ids = currItems.map(datum => datum.id)
 		ids.push(data.id)
