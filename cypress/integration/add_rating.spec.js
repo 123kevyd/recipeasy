409a39a961c8bb4ea2d237c00c2092e2b100dc20
Vinh Tran <vinhtran.k@gmail.com>
2022-04-11 18:04:52 -0500

    151: Create rating acceptance test

---
+++ cypress/integration/add_rating.spec.js
@@ -0,0 +1,28 @@
+describe(`Add A Rating`, () => {
+    it(`Should Fail Due to Missing Fields`, () => {
+        // // should reseed the db instead of randomly generating a name
+        cy.visit('http://localhost:3199/user/8', {timeout: 30000})
+        cy.get(`button`, {timeout: 10000}).contains('Recipes').click()
+        cy.get(`button`, {timeout: 10000}).contains('View').click()
+        cy.get(`button`, {timeout: 10000}).contains('Submit').click()
+        cy.on('window:alert', (text) => {
+            expect(text).to.contains('Error: Both difficulty and stars are required');
+        });
+    })
+
+    it(`Should Add A Rating`, () => {
+        const rnd = Math.floor(Math.random() * 100000)
+        cy.intercept(`/api/rating`).as('postAttempt')
+        cy.visit('http://localhost:3199/user/8', {timeout: 30000})
+        cy.get(`button`, {timeout: 10000}).contains('Recipes').click()
+        cy.get(`button`, {timeout: 10000}).contains('View').click()
+        cy.get(`#mui-28`, {timeout: 10000}).click({force: true})
+        cy.get(`#mui-34`, {timeout: 10000}).click({force: true})
+        cy.get('#mui-39', {timeout: 10000}).type(`${rnd}testreview :-)`)
+        cy.get(`button`, {timeout: 10000}).contains('Submit').click()
+        cy.wait('@postAttempt', {timeout: 10000}).then((interception) => {
+            assert.equal(interception.response.statusCode, 200, `post successful`)
+        })
+        cy.get('div', {timeout: 10000}).contains(`${rnd}testreview :-)`)
+    })
+})
